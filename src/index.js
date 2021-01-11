be8e8c0f52be92979ac93e896a3be913ceaff3a6
Ilia <iliapnmrvv@gmail.com>
2021-01-11 16:29:05 +0100

    New update

--- src/index.js
+++ src/index.js
@@ -10,24 +10,25 @@ import './index.css';
 
 const App = () => {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+    const todoData = [
+        { label: 'Сделать домашнюю работу', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ];
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+    return ( 
+      <div className = "todo-app" >
+        <AppHeader toDo = { 1 }
+                    done = { 3 }
+          /> 
+        <div className = "top-panel d-flex" >
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos = { todoData }/> 
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
+    );
 };
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+ReactDOM.render(< App/> ,
+    document.getElementById('root'));
