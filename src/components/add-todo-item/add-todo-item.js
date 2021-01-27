02b609595fefd6bc93c8a1360dfbf8ad744b0902
Ilia <iliapnmrvv@gmail.com>
2021-01-28 00:08:43 +0100

    Form submitting

--- src/components/add-todo-item/add-todo-item.js
+++ src/components/add-todo-item/add-todo-item.js
@@ -3,15 +3,37 @@ import React, {Component} from 'react';
 import './add-todo-item.css'
 export default class AddTodoItem extends Component {
 
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.onItemAdded(this.state.label)
+        this.setState({
+            label:''
+        })
+    }
+
     render() {
         return(
-        <div className="add-todo-item">
-            <button 
-                className = "btn btn-outline-secondary"
-                onClick={() => this.props.onItemAdded('hello world')}>
-                Add new item 
-            </button>
-        </div>
+            <form className="add-todo-item d-flex"
+                    onSubmit={this.onSubmit}>
+                <input type="text" 
+                    className="form-control" 
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done" 
+                    value={this.state.label}/>
+                <button className = "btn btn-outline-secondary">
+                    Add new item 
+                </button>
+            </form>
         )
     }
 }
